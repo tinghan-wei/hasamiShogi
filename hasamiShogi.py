5d956857ff1bc257183b727b1038fd8abd8a5035
ting <you@example.com>
2025-07-29 22:19:17 +0900

    Finish testing and fixing HS bugs

--- hasamiShogi.py
+++ hasamiShogi.py
@@ -1,3 +1,5 @@
+import copy
+
 BOARD_SIZE = 9
 EMPTY, BLACK, WHITE = '.', 'B', 'W'
 DIRECTIONS = [(1,0),(-1,0),(0,1),(0,-1)]
@@ -8,6 +10,34 @@ class HasamiShogi:
         for c in range(BOARD_SIZE):
             self.board[0][c] = BLACK
             self.board[-1][c] = WHITE
+        # track captures: how many pieces each color has captured
+        self.captures = {BLACK: 0, WHITE: 0}
+        # pending victory condition state
+        self.pending_leader = None
+        self.turn = BLACK
+
+    def set_board(self, board_list):
+        """
+        Set the board state directly for testing.
+        Expects a list of BOARD_SIZE lists/strings, each of length BOARD_SIZE,
+        containing only '.', 'B', or 'W'.
+        """
+        if len(board_list) != BOARD_SIZE:
+            raise ValueError(f"Board must have {BOARD_SIZE} rows")
+        new_board = []
+        for r, row in enumerate(board_list):
+            if len(row) != BOARD_SIZE:
+                raise ValueError(f"Row {r} must have {BOARD_SIZE} columns")
+            row_list = list(row)
+            for c, cell in enumerate(row_list):
+                if cell not in (EMPTY, BLACK, WHITE):
+                    raise ValueError(f"Invalid piece '{cell}' at ({r},{c})")
+            new_board.append(row_list)
+        self.board = new_board
+        # track captures: how many pieces each color has captured
+        self.captures = {BLACK: 0, WHITE: 0}
+        # pending victory condition state
+        self.pending_leader = None
         self.turn = BLACK
 
     def serialize(self):
@@ -63,64 +93,102 @@ class HasamiShogi:
         # 4. Path must be clear
         if not self.is_clear_path(r1, c1, r2, c2):
             return False
-        # 5. No suicide
+        # 5. No suicide, except when capturing
         for dr, dc in DIRECTIONS:
             r_pos, c_pos = r2 + dr, c2 + dc
             r_neg, c_neg = r2 - dr, c2 - dc
             if (self.in_bounds(r_pos, c_pos) and self.in_bounds(r_neg, c_neg) and
                 self.board[r_pos][c_pos] == opp and self.board[r_neg][c_neg] == opp):
-                return False
+                
+                boardCopy = copy.deepcopy(self.board)
+                total = 0
+                for dr1, dc1 in DIRECTIONS:
+                    total += self.capture_from(r2, c2, dr1, dc1, me, opp)
+                total += self.remove_dead_groups(opp)
+                self.board = copy.deepcopy(boardCopy)
+                if total == 0:
+                    return False
         return True
     
     def capture_from(self, r0, c0, dr, dc, me, opp):
-        r, c = r0+dr, c0+dc
+        r, c = r0 + dr, c0 + dc
         captured = []
-        while self.in_bounds(r,c) and self.board[r][c]==opp:
-            captured.append((r,c))
+        while self.in_bounds(r, c) and self.board[r][c] == opp:
+            captured.append((r, c))
             r += dr; c += dc
-        if self.in_bounds(r,c) and self.board[r][c]==me and captured:
-            for (rr,cc) in captured:
+        if self.in_bounds(r, c) and self.board[r][c] == me and captured:
+            for rr, cc in captured:
                 self.board[rr][cc] = EMPTY
+            return len(captured)
+        return 0
+
+    def remove_dead_groups(self, color):
+        # Go-like capture: any connected group of 'color' with no liberties
+        visited = set()
+        dead = []
+        for r in range(BOARD_SIZE):
+            for c in range(BOARD_SIZE):
+                if (r, c) not in visited and self.board[r][c] == color:
+                    # BFS to find the group
+                    queue = [(r, c)]
+                    group = [(r, c)]
+                    visited.add((r, c))
+                    has_liberty = False
+                    while queue:
+                        cr, cc = queue.pop()
+                        for dr, dc in DIRECTIONS:
+                            nr, nc = cr + dr, cc + dc
+                            if self.in_bounds(nr, nc):
+                                if self.board[nr][nc] == EMPTY:
+                                    has_liberty = True
+                                elif self.board[nr][nc] == color and (nr, nc) not in visited:
+                                    visited.add((nr, nc))
+                                    queue.append((nr, nc))
+                                    group.append((nr, nc))
+                    if not has_liberty:
+                        dead.extend(group)
+        # remove dead and return count
+        for (dr, dc) in dead:
+            self.board[dr][dc] = EMPTY
+        return len(dead)
 
     def apply_move(self, r1, c1, r2, c2, me):
-        """Validate, slide the piece like a rook, then perform captures."""
+        opp = BLACK if me == WHITE else WHITE
         if not self.is_legal_move(r1, c1, r2, c2, me):
-            raise ValueError(f"Illegal move: from {(r1,c1)} to {(r2,c2)}")
-
-        # 1. Slide
+            raise ValueError(f"Illegal move: from ({r1},{c1}) to ({r2},{c2})")
+        # perform slide
         self.board[r1][c1] = EMPTY
         self.board[r2][c2] = me
-
-        # 2. Flank‐capture in all four orthogonal directions
-        opp = BLACK if me == WHITE else WHITE
+        # track captures
+        total = 0
         for dr, dc in DIRECTIONS:
-            self.capture_from(r2, c2, dr, dc, me, opp)
+            total += self.capture_from(r2, c2, dr, dc, me, opp)
+            print(self.serialize())
+        total += self.remove_dead_groups(opp)
+        print(self.serialize())
+        self.captures[me] += total
         
+        # update pending_leader in case of leading 3 captures
+        cm, co = self.captures[me], self.captures[opp]
+        lead = cm - co
+        if lead >= 3 and self.pending_leader == None:
+            self.pending_leader = me
+        elif lead >= -2 and self.pending_leader == opp:
+            self.pending_leader = None
+
+        # record last mover
         self.last_move = (r1,c1,r2,c2)
         self.turn = opp
 
     def is_game_over(self):
-        blacks = sum(r.count(BLACK) for r in self.board)
-        whites = sum(r.count(WHITE) for r in self.board)
-        if blacks <= 1: return WHITE
-        if whites <= 1: return BLACK
+        # player to play next
+        opp = self.turn
+        # player who just moved
+        me = BLACK if opp == WHITE else WHITE
+        cm = self.captures[me]
+        # 1. First to 5 captures
+        if cm >= 5:
+            return me
+        elif self.pending_leader is opp:
+            return self.pending_leader
         return None
-
-    def set_board(self, board_list):
-        """
-        Set the board state directly for testing.
-        Expects a list of BOARD_SIZE lists/strings, each of length BOARD_SIZE,
-        containing only '.', 'B', or 'W'.
-        """
-        if len(board_list) != BOARD_SIZE:
-            raise ValueError(f"Board must have {BOARD_SIZE} rows")
-        new_board = []
-        for r, row in enumerate(board_list):
-            if len(row) != BOARD_SIZE:
-                raise ValueError(f"Row {r} must have {BOARD_SIZE} columns")
-            row_list = list(row)
-            for c, cell in enumerate(row_list):
-                if cell not in (EMPTY, BLACK, WHITE):
-                    raise ValueError(f"Invalid piece '{cell}' at ({r},{c})")
-            new_board.append(row_list)
-        self.board = new_board
--- hasamiTest.py
+++ hasamiTest.py
@@ -1,20 +1,101 @@
+import unittest
 import hasamiShogi
 
 game = hasamiShogi.HasamiShogi()
 
-board = [
-    "BBBB.BBBB",
-    ".........",
-    ".........",
-    ".........",
-    ".........",
-    ".........",
-    ".........",
-    ".........",
-    "WWWWWWWWW"
-]
-game.set_board(board)
-print(game.serialize())
+class TestHasamiShogi(unittest.TestCase):
+    def test_victory(self):
+        board = [
+            ".BBB....B",
+            ".........",
+            ".........",
+            ".......B.",
+            "....BBBW.",
+            "B........",
+            ".........",
+            ".........",
+            "WWWWWWW.W"
+        ]
+        game.set_board(board)
+        game.apply_move(8,3,4,3, hasamiShogi.WHITE)
+        player = game.is_game_over()
+        self.assertEqual(player, None)
 
-game.apply_move(8,4,0,4, hasamiShogi.WHITE)
-print(game.serialize())
+        game.apply_move(0,8,4,8, hasamiShogi.BLACK)
+        player = game.is_game_over()
+        self.assertEqual(player, hasamiShogi.WHITE)
+
+        board = [
+            #012345678
+            ".BBB....B", # 0
+            ".........", # 1
+            ".........", # 2
+            ".......BW", # 3
+            "....BBBW.", # 4
+            "B........", # 5
+            ".........", # 6
+            ".........", # 7
+            "WWWWWWW.."
+        ]
+        game.set_board(board)
+        # W leads by 3
+        game.apply_move(8,3,4,3, hasamiShogi.WHITE)
+        player = game.is_game_over()
+        self.assertEqual(player, None)
+
+        # B captures 1 to undo the lead
+        game.apply_move(5,0,5,7, hasamiShogi.BLACK)
+        player = game.is_game_over()
+        self.assertEqual(player, None)
+
+        # W captures 1 again to get the lead again
+        game.apply_move(8,6,3,6, hasamiShogi.WHITE)
+        player = game.is_game_over()
+        self.assertEqual(player, None)
+
+        # B cannot capture back, W wins
+        game.apply_move(0,8,2,8, hasamiShogi.BLACK)
+        player = game.is_game_over()
+        self.assertEqual(player, hasamiShogi.WHITE)
+
+    def test_corner(self):
+        board = [
+            #012345678
+            "WWWB.WB.W",
+            ".BB...B.B",
+            ".........",
+            ".........",
+            "......B..",
+            ".........",
+            "B........",
+            ".........",
+            "........."
+        ]
+        game.set_board(board)
+        game.apply_move(1,6,1,7, hasamiShogi.BLACK)
+        self.assertEqual(game.board[0][7], hasamiShogi.EMPTY)
+        self.assertEqual(game.board[0][8], hasamiShogi.WHITE)
+        
+        # white captures after first move
+        game.apply_move(0,8,0,7, hasamiShogi.WHITE)
+        game.apply_move(1,8,0,8, hasamiShogi.BLACK)
+        self.assertEqual(game.board[0][7], hasamiShogi.WHITE)
+
+        # black captures back
+        game.apply_move(4,6,0,6, hasamiShogi.BLACK)
+        self.assertEqual(game.board[0][7], hasamiShogi.EMPTY)
+        
+        game.apply_move(0,5,1,5, hasamiShogi.WHITE)
+        game.apply_move(6,0,1,0, hasamiShogi.BLACK)
+        self.assertEqual(game.board[0][0], hasamiShogi.EMPTY)
+        self.assertEqual(game.board[0][1], hasamiShogi.EMPTY)
+        self.assertEqual(game.board[0][2], hasamiShogi.EMPTY)
+        player = game.is_game_over()
+
+        game.apply_move(1,5,2,5, hasamiShogi.WHITE)
+        # print(game.serialize())
+        player = game.is_game_over()
+        self.assertEqual(player, hasamiShogi.BLACK)
+
+if __name__ == '__main__':
+    unittest.main()
