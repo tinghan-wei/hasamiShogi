06739f6c80c41b3125fceaad4ffc599ed01cdb0c
ting <tinghan.wei@kochi-tech.ac.jp>
2026-07-15 11:29:23 +0900

    Fix shell script in platform

--- platform/dev_start.sh
+++ platform/dev_start.sh
@@ -1,3 +1,3 @@
-!/bin/bash
+#!/bin/bash
 
 USE_DOCKER=false TOURNAMENT_INTERVAL=60 uv run python app.py
--- platform/prod_start.sh
+++ platform/prod_start.sh
@@ -1,6 +1,6 @@
-!/bin/bash
+#!/bin/bash
 
 docker pull python:3.11-slim
 uv sync --extra prod
-USE_DOCKER=true TOURNAMENT_INTERVAL=60 \
+USE_DOCKER=true TOURNAMENT_INTERVAL=3600 \
   uv run gunicorn -w 1 -b 0.0.0.0:5000 --threads 8 app:app
